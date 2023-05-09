94edaa27d3bdcc060a4bbf9fcee5a738b0ac962d
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-05-09 13:23:25 +0100

    random

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Logo from "./images/favicon.png"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
