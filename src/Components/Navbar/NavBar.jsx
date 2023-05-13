6dad7f6ca5bc04f739f61fda5a3badda88166675
odediran philip oluseyi <philipoluseyi@gmail.com>
2023-05-13 12:07:34 +0100

    done with nav

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -15,54 +15,58 @@ export default function NavBar() {
     }
 
     return(
+        <>
+
         <nav>
-        <div className='nav'>
-            <div className="brand">
-                <img src={'https://res.cloudinary.com/do5wu6ikf/image/upload/v1683923725/starpenzu/Starlogo_1_hhmgj6.svg'} className='navLogo'  alt="brand logo"/>
-                <div className="lgText">
-                    Starpenzu
+                <div className="brand">
+                    <img src={'https://res.cloudinary.com/do5wu6ikf/image/upload/v1683923725/starpenzu/Starlogo_1_hhmgj6.svg'} className='navLogo'  alt="brand logo"/>
+                    <div className="lgText">
+                        Starpenzu
+                    </div>
                 </div>
-            </div>
 
-            <div className="harmBurger">
-                {!hamburger? <img src={"https://res.cloudinary.com/do5wu6ikf/image/upload/v1683922718/starpenzu/Vector_hgh9j9.svg"}
-                      className='hamburgerImg' onClick={handlehamburger} alt="ham"/> :
-                    <img src={"https://res.cloudinary.com/do5wu6ikf/image/upload/v1683922753/starpenzu/Vector_r4aqp6.svg"}
-                                                                                            className='hamburgerImg'  onClick={handlehamburger} alt="ham"/>}
-            </div>
+                <div className="harmBurger">
+                    {!hamburger? <img src={"https://res.cloudinary.com/do5wu6ikf/image/upload/v1683922718/starpenzu/Vector_hgh9j9.svg"}
+                          className='hamburgerImg' onClick={handlehamburger} alt="ham"/> :
+                        <img src={"https://res.cloudinary.com/do5wu6ikf/image/upload/v1683922753/starpenzu/Vector_r4aqp6.svg"}
+                                                                                                className='hamburgerImg'  onClick={handlehamburger} alt="ham"/>}
+                </div>
 
-        {/*show on 1024px*/}
+                {/*show on 1024px*/}
 
-            <div className="navItems">
-                <ul>
-                    <li className='navItemActive'>Home</li>
-                    <li>Courses</li>
-                    <li>Contact Us</li>
-                    <li>About Us</li>
-                    <li>
-                        <Button className="registerButton" name='Register'/>
-                    </li>
-                    <li>
-                       <Button className="loginButton" name='Login'/>
-                    </li>
-                </ul>
-            </div>
+                <div className="navItems">
+                    <ul>
+                        <li className='navItemActive'>Home</li>
+                        <li>Courses</li>
+                        <li>Contact Us</li>
+                        <li>About Us</li>
+                        <li>
+                            <Button className="registerButton" name='Register'/>
+                        </li>
+                        <li>
+                           <Button className="loginButton" name='Login'/>
+                        </li>
+                    </ul>
+                </div>
 
-        </div>
+
+       </nav>
+
+        <div>
             {hamburger &&
                 <div className={`harmBurgerDropDown`}>
-                     <ul>
-                         <li className='hamBurgerActive'>Home</li>
-                         <li>Courses</li>
-                         <li>Contact Us</li>
-                         <li>About Us</li>
-                         <li>Register</li>
-                         <li>Login</li>
-                     </ul>
+                    <ul>
+                        <li className='hamBurgerActive'>Home</li>
+                        <li>Courses</li>
+                        <li>Contact Us</li>
+                        <li>About Us</li>
+                        <li>Register</li>
+                        <li>Login</li>
+                    </ul>
                 </div>
             }
+        </div>
 
-
-        </nav>
+        </>
     )
 }
