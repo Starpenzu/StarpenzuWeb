b057205e97d782a0c504fe66b7f597ea4446fd06
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-05-16 07:56:10 +0100

    course section  done pheww

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,38 +1,52 @@
 import React from 'react';
+import Slider from 'react-slick';
 import CoursesComponent from './CoursesComponent'
 
 
 export default function Courses(){
-    return(
-        <div className='coursesContainer'>
-            <div className="feaCourses">
-                <div className="courcesTexts">
-                    <div className="headText">
-                        Featured courses
+
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        };
+
+        return (
+            <>
+
+                <div className='coursesContainer'>
+                    <div className="feaCourses">
+                        <div className="courcesTexts">
+                            <div className="headText">
+                                Featured courses
+                            </div>
+
+                            <div className="subText">
+                                Discover Your Perfect Program In Our Courses.
+                            </div>
+                        </div>
+
+                        <div className="viewCouses">
+                            <button>View All</button>
+                        </div>
                     </div>
 
-                    <div className="subText">
-                        Discover Your Perfect Program In Our Courses.
-                    </div>
+
+
                 </div>
 
-                <div className="viewCouses">
-                    <button>View All</button>
+                <div className="courcesContainer2">
+                    <CoursesComponent/>
+                    <CoursesComponent/>
+                    <CoursesComponent/>
+
+                    <CoursesComponent/>
+                    <CoursesComponent/>
+                    <CoursesComponent/>
                 </div>
-            </div>
 
-            <div className="courcesContainer2">
-                <CoursesComponent/>
-                <CoursesComponent/>
-                <CoursesComponent/>
-                <CoursesComponent/>
-                <CoursesComponent/>
-                <CoursesComponent/>
-                <CoursesComponent/>
-                <CoursesComponent/>
-                <CoursesComponent/>
-            </div>
-
-        </div>
-    )
-}
+            </>
+        );
+    }
