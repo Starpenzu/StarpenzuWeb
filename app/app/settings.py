b0ee3df6063d745f4b82d31ebfcf4e03f201b423
ajadi <ajadimarvellousgo@gmail.com>
2023-07-04 17:44:09 +0100

    Hosting

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,16 +24,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY','changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',0)))
 
-DEBUG = True
-ALLOWED_HOSTS = ['localhost',
-                 '127.0.0.1',
-                 'http://localhost:8000',]
-# ALLOWED_HOSTS.extend(
-#     filter(
-#         None,
-#         os.environ.get('ALLOWED_HOSTS','').split(',')
-#     )
-# )
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS','').split(',')
+    )
+)
 # Application definition
 
 INSTALLED_APPS = [
